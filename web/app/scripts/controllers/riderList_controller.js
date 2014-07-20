206abf70d4a8394bcf5b2b57f16ff9c12b058f5a
Arron Hartley <arronhartley@gmail.com>
2014-07-20 09:53:22 +0100

    Defaulting to show the first 50 results. Added code to show more or all.

--- web/app/scripts/controllers/riderList_controller.js
+++ web/app/scripts/controllers/riderList_controller.js
@@ -4,13 +4,47 @@ App.RiderListController = Ember.ArrayController.extend(
 	@property sortProperties
 	Properties dictating the arrangedContent's sort order.
 	*/
-	sortProperties: ["weight"],
+	sortProperties: ["age"],
 
 	/**
 	@property sortAscending
 	The arrangedContent's sort direction.
 	*/
 	sortAscending: true,
+
+	/**
+	@property contentSetSize
+	The number of results to show
+	*/
+	contentSetSize: 50,
+
+	/**
+	@method limitedContent
+	Returns a limited set of results from the sorted array.
+	*/
+	limitedContent: function()
+	{
+		return this.get("arrangedContent").slice(0, this.get("contentSetSize"));
+	}.property("arrangedContent.[]", "sortProperties", "sortAscending", "contentSetSize"),
+
+	contentSetSizeChanged: function()
+	{
+		Ember.run.scheduleOnce('afterRender', this, function()
+		{
+    		$('[data-spy="scroll"]').each(function()
+			{
+				var $spy = $(this).scrollspy('refresh')
+			});
+  		});
+	}.observes("contentSetSize"),
+
+	shouldShowMore: function()
+	{
+		if (this.get("contentSetSize") == this.get("content.length"))
+			return false;
+
+		return true;
+	}.property("contentSetSize"),
 
 	actions:
 	{
@@ -26,6 +60,17 @@ App.RiderListController = Ember.ArrayController.extend(
 				this.set("sortProperties", [column]);	
 				this.get("sortAscending", true);
 			}
+		},
+
+		showMore: function()
+		{
+			var currentSize = this.get("contentSetSize");
+			this.set("contentSetSize", currentSize + 50);
+		},
+
+		showAll: function()
+		{
+			this.set("contentSetSize", this.get("content.length"));	
 		}
 	}
 });
